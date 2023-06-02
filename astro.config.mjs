481ec78f5a7c1723a901c7c8ed1de4148f45cae2
Data Salaryman <datasalaryman@protonmail.com>
2023-06-02 18:47:19 +0800

    build: add other astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), image(), mdx(), sitemap()],
+  output: "server",
+  adapter: vercel()
+});
